e8f5cb8ee39bf8e64a2fe23f94764ea22a8e580d
Amogha V A <amogha.va.iyer@gmail.com>
2025-09-15 01:04:49 +0530

    started with auth

---
+++ models/userModel.js
@@ -0,0 +1,50 @@
+import { verify } from "crypto";
+import mongoose from "mongoose";
+import { type } from "os";
+import { boolean, date } from "zod";
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    match: [/.+@.+\..+/, "Please enter a valid email address"],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+  phone: {
+    type: String,
+    required: true,
+    match: [/^\d{10}$/, "Please enter a valid phone number"],
+  },
+  governmentId: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+
+  forgotPasswordToken: String,
+  forgotPasswordTokenExpiry: Date,
+  verifyToken: String,
+  verifyTokenExpiry: Date,
+});
